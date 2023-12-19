908cace66d876c25aa4a2adf8830b27c34623277
Natalia Oskina <oskina@google.com>
2023-12-19 16:56:00 +0000

    Revert "Merge branch 'main' into development"
    This reverts commit 6f1d573476ccc584ffd44a1c23a32133a7bdb7e1.
    
    Reason for revert: main wasn't updated 
    
    Change-Id: I512e650bf02b76d82368bdcdf11ee9c7b6b722af

--- terraform/api_activation.tf
+++ terraform/api_activation.tf
@@ -64,3 +64,21 @@ resource "google_project_service" "artifactregistry" {
   service = "artifactregistry.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "iam" {
+  project = var.project_id
+  service   = "iam.googleapis.com"
+  disable_on_destroy = false
+} 
+
+resource "google_project_service" "cloudresourcemanager" {
+  project = var.project_id
+  service   = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudrun" {
+  project = var.project_id
+  service   = "run.googleapis.com"
+  disable_on_destroy = false
+}
